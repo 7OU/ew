e30c04ddaf303241a4bb627090db4981465bf9f2
Evan <you@example.com>
2020-01-12 21:52:46 -0500

    Cleaned up code

--- ew.js
+++ ew.js
@@ -1,37 +1,33 @@
 const isRoot = process.getuid && process.getuid() === 0;
 const fs = require('fs');
-const download = require('download-file')
-const repoOptions = {
-    directory: "/etc/ew/",
-    filename: "packages.json"
-}
-var packageDL;
+const download = require('download-file');
 
-// Check for ioot
-if (!isRoot) { return console.log("You must be root to excecute this command!") }
-// Check for lock file
+is (!isRoot) return console.log("You must be root to excecute this command!");
 if (!fs.existsSync("/etc/ew")) fs.mkdirSync("/etc/ew");
-if (fs.existsSync("/etc/ew/lock")) { return console.log("/etc/ew/lock exists, cancelling.") }
-// Create lock file
-fs.writeFileSync('/etc/ew/lock', 'ew package manager lock\n');
-console.log("Created lock file.")
-// Download package list
-function updatePackageList(){
-    process.stdout.write("Updating Package List...")
-    download("https://ew.cumbox.best/packages.json", dlOptions, function(err){
-        if (err) throw err;
-    process.stdout.write(" Done!\n");
+//if (fs.existsSync("/etc/ew/lock")) return console.log("/etc/ew/lock exists, cancelling.");
+//fs.writeFileSync('/etc/ew/lock', 'ew package manager lock\n');
+//console.log("Created lock file.");
+if (!fs.existsSync("/etc/ew/packages.json")) { updatePackageList(); fs.writeFileSync('/etc/ew/packages.json', '{}') };
+
+
+if (process.argv[2]) updatePackageList();
+if (process.argv[2]) install(process.argv[2]);
+if (process.argv[2]) uninstall(process.argv[2]);
+
+function updatePackageList() {
+    process.stdout.write("Updating package list... ");
+    download("https://ew.cumbox.best/packages.json", { directory: "/etc/ew", filename: "packages.json" }, function (err) {
+        if (err) throw err
+        process.stdout.write(Done!\n);
     });
 };
-// Updates packages, and then creates the variable for the package list json.
-if (!fs.existsSync("/etc/ew/packages.json")) updatePackageList();
-const packageList = require ("/etc/ew/packages.json");
-// Updates package list if the argument is -r
-if (process.argv[2] === '-r') updatePackageList();
 
-if (process.argv[2] === '-i') {
-    console.log(packageList.process.argv[3]);
-    if (packageList.[process.argv[3]] === undefined) return console.log("no");
-}
+function install(package) {
+    console.log(package);
+};
 
-fs.unlinkSync('/etc/ew/lock')
+function uninstall(package) {
+    console.log(package);
+};
+
+fs.unlinkSync('/etc/ew/lock');
