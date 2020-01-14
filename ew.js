2044977e3a3b707288034aabcf0cb5c879744ce7
Evan <you@example.com>
2020-01-13 21:20:41 -0500

    Added install

--- ew.js
+++ ew.js
@@ -1,33 +1,41 @@
 const isRoot = process.getuid && process.getuid() === 0;
 const fs = require('fs');
-const download = require('download-file');
+const download = require('download');
+const { exec } = require('child_process');
+const targz = require('targz');
 
-is (!isRoot) return console.log("You must be root to excecute this command!");
+if (!isRoot) return console.log("You must be root to excecute this command!");
 if (!fs.existsSync("/etc/ew")) fs.mkdirSync("/etc/ew");
 //if (fs.existsSync("/etc/ew/lock")) return console.log("/etc/ew/lock exists, cancelling.");
 //fs.writeFileSync('/etc/ew/lock', 'ew package manager lock\n');
 //console.log("Created lock file.");
 if (!fs.existsSync("/etc/ew/packages.json")) { updatePackageList(); fs.writeFileSync('/etc/ew/packages.json', '{}') };
 
-
-if (process.argv[2]) updatePackageList();
-if (process.argv[2]) install(process.argv[2]);
-if (process.argv[2]) uninstall(process.argv[2]);
+if (process.argv[2] === '-r') updatePackageList();
+if (process.argv[2] === '-i') install(process.argv[3]);
+if (process.argv[2] === '-u') uninstall(process.argv[3]);
 
 function updatePackageList() {
     process.stdout.write("Updating package list... ");
     download("https://ew.cumbox.best/packages.json", { directory: "/etc/ew", filename: "packages.json" }, function (err) {
-        if (err) throw err
-        process.stdout.write(Done!\n);
+        if (err) throw err;
+        process.stdout.write("Done!\n");
     });
 };
 
 function install(package) {
-    console.log(package);
+    var packageList = require("/etc/ew/packages.json");
+    var packageURL = packageList[package]
+    console.log(packageURL);
+    process.stdout.write(`Downloading ${package}`);
+    download(packageURL, '/tmp').then(() => {
+        targz.decompress({ src: `/tmp/${package}.ew.tar.gz`, dest: '/' });
+//        exec(`tar xzvf /tmp/${package}.ew.tar.gz -C /`);
+    });
 };
 
 function uninstall(package) {
-    console.log(package);
-};
+    console.log(package)
+}
 
-fs.unlinkSync('/etc/ew/lock');
+//fs.unlinkSync('/etc/ew/lock');
