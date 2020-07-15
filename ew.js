4b46bc1c7c303eb52b0cf9672f91eec00f76fcd9
Matt <mattstar45@gmail.com>
2020-07-15 02:33:02 -0400

    add removal
    add a removal function

--- ew.js
+++ ew.js
@@ -1,13 +1,12 @@
 const isRoot = process.getuid && process.getuid() === 0;
-const fs = require('fs');
+const fs = require('fs-extra');
 const download = require('download-file');
 const targz = require('targz');
 
 if (!isRoot) return console.log("You must be root to excecute this command!");
 if (!fs.existsSync("/etc/ew")) fs.mkdirSync("/etc/ew");
 if (!fs.existsSync("/etc/ew/installed")) fs.mkdirSync("/etc/ew/installed");
-//make an archive to help with other functions
-if (!fs.existsSync("/etc/ew/archive")) fs.mkdirSync("/etc/ew/archive");
+
 if (!fs.existsSync("/etc/ew/source")) {
     fs.mkdirSync("/etc/ew/source");
     fs.writeFileSync("/etc/ew/sources.list");
@@ -26,6 +25,7 @@ if (process.argv[2] === '-i') install(process.argv[3]);
 if (process.argv[2] === '-u') uninstall(process.argv[3]);
 if (process.argv[2] === '-a') addsrc(process.argv[3]);
 if (process.argv[2] === '-h') help();
+if (process.argv[2] === '-l') update();
 
 function updatePackageList() {
     process.stdout.write("Updating package list... ");
@@ -43,22 +43,19 @@ function help() {
     console.log("-u: uninstalls a package");
     console.log("-a: add a source");
     console.log("-h: prints list of commands");
+    console.log("-l: update ew");
 }
 
 function install(package) {
     var packageList = require("/etc/ew/packages.json");
-    var packageURL = packageList[package]
+    var packageURL = packageList[package];
     console.log(packageURL);
     process.stdout.write(`Downloading ${package}... `);
     download(packageURL, { directory: "/tmp", filename: `${package}.ew.tar.gz` }, function() {
         targz.decompress({ src: `/tmp/${package}.ew.tar.gz`, dest: '/' }, function() {
-            fs.renameSync(`/tmp/${package}.ew.tar.gz`, `/etc/ew/archive/${package}.ew.tar.gz`);
-            //check again if it exists 
-            if (!fs.existsSync(`/etc/ew/archive/${package}.ew.tar.gz`)) {
-                //force removal of it from tmp
-                fs.unlinkSync(`/tmp/${package}.ew.tar.gz`);
-                console.log(`warning, ${package}'s archive may or may not exist.`);
-            }
+
+            fs.unlinkSync(`/tmp/${package}.ew.tar.gz`);
+
         });
         process.stdout.write("Done!\n");
         console.log(`Installed ${package}!`);
@@ -67,10 +64,23 @@ function install(package) {
 };
 
 function uninstall(package) {
-    return console.log("uninstall is not finished yet.");
+    process.stdout.write(`removing ${package}....`);
+    if (!fs.existsSync(`/etc/ew/installed/${package}.json`) && !fs.existsSync(`/etc/ew/archive/${package}.ew.tar.gz`)) return console.log("package already removed!");
+    let json = require(`/etc/ew/installed/${package}.json`);
+    let directories = json.directoriesToDelete;
+    let directorycount = directories.length;
+    for (let file = directories; directorycount > 0; directorycount--) {
+        let newfile = JSON.stringify(file);
+        newfile = newfile.replace(/[\[\]"]/g, "");
+        fs.removeSync(newfile);
+    }
+    fs.unlinkSync(`/etc/ew/installed/${package}.json`);
+    process.stdout.write(`done!\n`);
+    process.stdout.write(`Successfully  uninstalled ${package}!\n`);
 }
 
 function addsrc(src) {
     return console.log("this function is unfinished");
 }
+
 //fs.unlinkSync('/etc/ew/lock');
