221c56f93d1968aeaf0420034ee813b97dcd0000
Matt <mattsextendere@outlook.com>
2020-07-18 00:24:06 -0400

    fix the check in the uninstall function
    this is a very weird bug, dont know the cause either, but fixed by getting the oldest working version of ew, and adding the newer changes to it(?) and it fixed it so i dont know.

--- ew.js
+++ ew.js
@@ -6,12 +6,13 @@ const targz = require('targz');
 if (!isRoot) return console.log("You must be root to excecute this command!");
 if (!fs.existsSync("/etc/ew")) fs.mkdirSync("/etc/ew");
 if (!fs.existsSync("/etc/ew/installed")) fs.mkdirSync("/etc/ew/installed");
+//make an archive to help with other functions
+if (!fs.existsSync("/etc/ew/archive")) fs.mkdirSync("/etc/ew/archive");
 
 if (!fs.existsSync("/etc/ew/source")) {
     fs.mkdirSync("/etc/ew/source");
     fs.writeFileSync("/etc/ew/sources.list");
 }
-
 //if (fs.existsSync("/etc/ew/lock")) return console.log("/etc/ew/lock exists, cancelling.");
 //fs.writeFileSync('/etc/ew/lock', 'ew package manager lock\n');
 //console.log("Created lock file.");
@@ -19,11 +20,9 @@ if (!fs.existsSync("/etc/ew/packages.json")) {
     updatePackageList();
     fs.writeFileSync('/etc/ew/packages.json', '{}')
 };
-
 if (process.argv[2] === '-r') updatePackageList();
 if (process.argv[2] === '-i') install(process.argv[3]);
 if (process.argv[2] === '-u') uninstall(process.argv[3]);
-if (process.argv[2] === '-a') addsrc(process.argv[3]);
 if (process.argv[2] === '-h') help();
 
 function updatePackageList() {
@@ -40,13 +39,14 @@ function help() {
     console.log("-r: reload package list");
     console.log("-i: installs a package");
     console.log("-u: uninstalls a package");
-    console.log("-a: add a source");
     console.log("-h: prints list of commands");
 }
 
 function install(package) {
     var packageList = require("/etc/ew/packages.json");
+    var packageURL = packageList[package]
     var packageURL = packageList[package];
+    console.log(packageURL);
     process.stdout.write(`Downloading ${package}... `);
     download(packageURL, { directory: "/tmp", filename: `${package}.ew.tar.gz` }, function() {
         targz.decompress({ src: `/tmp/${package}.ew.tar.gz`, dest: '/' }, function() {
@@ -81,11 +81,8 @@ function uninstall(package) {
         newfile = newfile.replace(/[\[\]"]/g, "");
         fs.removeSync(newfile);
     }
-    process.stdout.write("done!\n");
-    console.log(`successfully uninstalled ${package}`);
-}
-
-function addsrc(src) {
-    return console.log("this function is unfinished");
+    fs.unlinkSync(`/etc/ew/installed/${package}.json`);
+    process.stdout.write(`done!\n`);
+    process.stdout.write(`Successfully  uninstalled ${package}!\n`);
 }
 //fs.unlinkSync('/etc/ew/lock');
