4ed4689fe77d94e2a85b9863bbefd2f60dbc7338
Matt <mattsextendere@outlook.com>
2020-07-24 04:18:25 -0400

    packagelist function and better input handling

--- ew.js
+++ ew.js
@@ -18,6 +18,7 @@ if (process.argv[2] === '-r') updatePackageList();
 if (process.argv[2] === '-i') install(process.argv[3]);
 if (process.argv[2] === '-u') uninstall(process.argv[3]);
 if (process.argv[2] === '-h') help();
+if (process.argv[2] === '-pl') packagelist();
 if (!process.argv[2]) help();
 
 function updatePackageList() {
@@ -29,12 +30,16 @@ function updatePackageList() {
 };
 
 function help() {
-    console.log("here are the list of commands available for ew package manager:\n \n -r: reload package list\n -i: installs a package\n -u: uninstalls a package\n -h: prints list of commands\n");
+    console.log("list of commands available for ew package manager:\n \n -r: reload package list\n -i: installs a package\n -u: uninstalls a package\n -pl: shows the list of packages that can be installed\n -h: prints list of commands\n");
 }
 
 function install(package) {
+    //check if the package arg exists
+    if (!process.argv[3]) return console.log("Please specify a package to install.");
     var packageList = require("/etc/ew/packages.json");
     var packageURL = packageList[package];
+    //check if its a valid package aswell..
+    if (!packageURL) return console.log("Invalid package specified.");
     process.stdout.write(`Downloading ${package}... `);
     download(packageURL, { directory: "/tmp", filename: `${package}.ew.tar.gz` }, function() {
         targz.decompress({ src: `/tmp/${package}.ew.tar.gz`, dest: '/' }, function() {
@@ -60,8 +65,9 @@ function install(package) {
 };
 
 function uninstall(package) {
+    if (!process.argv[3]) return console.log("Please specify a package to uninstall.");
     process.stdout.write(`removing ${package}....`);
-    if (!fs.existsSync(`/etc/ew/installed/${package}.json`)) return console.log("package already removed!");
+    if (!fs.existsSync(`/etc/ew/installed/${package}.json`)) return console.log("the specified package has already been removed or not installed.");
     let json = require(`/etc/ew/installed/${package}.json`);
     let directories = json.directoriesToDelete;
     let directorycount = directories.length;
@@ -73,5 +79,12 @@ function uninstall(package) {
     fs.unlinkSync(`/etc/ew/installed/${package}.json`);
     process.stdout.write(`done!\n`);
     process.stdout.write(`Successfully  uninstalled ${package}!\n`);
+}
+
+function packagelist() {
+    const json = require('/etc//ew/packages.json');
+    var packagecount = Object.keys(json).length;
+    console.log(packagecount + ' packages are available to be downloaded: ');
+    console.log(Object.keys(json));
 }
 //fs.unlinkSync('/etc/ew/lock');
